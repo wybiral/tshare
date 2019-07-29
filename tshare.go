a225d60f6cc4e84b900d1ba5245f49a3d07dbdf1
davy <davy.wybiral@gmail.com>
2019-07-28 23:54:29 -0500

    rewrite for streaming io

--- tshare.go
+++ tshare.go
@@ -1,92 +1,230 @@
+// Package tshare implements methods for (2,3) threshold secret sharing for
+// splitting secrets into three shares. None of the shares alone give away any
+// information about the secret (other than the length) but any combination of
+// two shares is able to fully recover the secret.
 package tshare
 
 import (
 	"crypto/rand"
 	"errors"
+	"io"
 )
 
-// Secrets are split bytewise into three shares using the following rule:
-//
+const bufferSize = 1024
+
+// Split reads from reader and splits data using (2,3) threshold secret sharing
+// algorithm, writing each share to the respective writer.
+func Split(r io.Reader, w0 io.Writer, w1 io.Writer, w2 io.Writer) error {
+	m := make([]byte, bufferSize)
+	s0 := make([]byte, bufferSize)
+	s1 := make([]byte, bufferSize)
+	s2 := make([]byte, bufferSize)
+	for {
+		n, _ := r.Read(m)
+		if n == 0 {
+			break
+		}
+		err := SplitBytes(m, s0, s1, s2)
+		if err != nil {
+			return err
+		}
+		_, err = w0.Write(s0[:n])
+		if err != nil {
+			return err
+		}
+		_, err = w1.Write(s1[:n])
+		if err != nil {
+			return err
+		}
+		_, err = w2.Write(s2[:n])
+		if err != nil {
+			return err
+		}
+	}
+	return nil
+}
+
+// Join recovers a secret message from two or more shares. Any one of the
+// supplied readers can be nil and the secret will still be recovered as long as
+// at least two are used. The reconstructed secret is written to w.
+func Join(w io.Writer, r0 io.Reader, r1 io.Reader, r2 io.Reader) error {
+	var m, s0, s1, s2 []byte
+	m = make([]byte, bufferSize)
+	if r0 != nil {
+		s0 = make([]byte, bufferSize)
+	}
+	if r1 != nil {
+		s1 = make([]byte, bufferSize)
+	}
+	if r2 != nil {
+		s2 = make([]byte, bufferSize)
+	}
+	for {
+		n, err := read(s0, s1, s2, r0, r1, r2)
+		if err != nil {
+			return err
+		}
+		if n == 0 {
+			break
+		}
+		var t0, t1, t2 []byte
+		if s0 != nil {
+			t0 = s0[:n]
+		}
+		if s1 != nil {
+			t1 = s1[:n]
+		}
+		if s2 != nil {
+			t2 = s2[:n]
+		}
+		err = JoinBytes(m, t0, t1, t2)
+		if err != nil {
+			return err
+		}
+		_, err = w.Write(m[:n])
+		if err != nil {
+			return err
+		}
+	}
+	return nil
+}
+
+// read from all readers where the corresponding share array isn't nil. Returns
+// number of bytes read, ensuring that they match and that at least two of the
+// shares are non-nil. Does not return EOF error so consumer should check byte
+// count for 0 to end stream.
+func read(s0, s1, s2 []byte, r0, r1, r2 io.Reader) (int, error) {
+	var n0, n1, n2 int
+	if s0 != nil {
+		n0, _ = io.ReadFull(r0, s0)
+	}
+	if s1 != nil {
+		n1, _ = io.ReadFull(r1, s1)
+	}
+	if s2 != nil {
+		n2, _ = io.ReadFull(r2, s2)
+	}
+	// verify share sizes match
+	if s0 != nil && s1 != nil && s2 != nil {
+		if n0 != n1 && n1 != n2 {
+			return 0, errors.New("share sizes must match")
+		}
+		return n0, nil
+	}
+	if s0 != nil && s1 != nil {
+		if n0 != n1 {
+			return 0, errors.New("share sizes must match")
+		}
+		return n0, nil
+	}
+	if s0 != nil && s2 != nil {
+		if n0 != n2 {
+			return 0, errors.New("share sizes must match")
+		}
+		return n0, nil
+	}
+	if s1 != nil && s2 != nil {
+		if n1 != n2 {
+			return 0, errors.New("share sizes must match")
+		}
+		return n1, nil
+	}
+	// less than two shares supplied are non-nil
+	return 0, errors.New("insufficient combination of shares")
+}
+
+// SplitBytes splits m into 3 shares using 2,3 threshold secret sharing
+// algorithm defined by:
 // m = secret byte with bits [m7 m6 m5 m4 m3 m2 m1 m0]
 // r = random byte
 // s0 = [ 0  0  0  0 m7 m6 m5 m4] ^ r
 // s1 = [m3 m2 m1 m0  0  0  0  0] ^ r
 // s2 = [m7 m6 m5 m4 m3 m2 m1 m0] ^ r
-//
-// The first byte of each share is a tag to denote which share it is.
-func Split(m []byte) ([][]byte, error) {
+func SplitBytes(m, s0, s1, s2 []byte) error {
 	n := len(m)
 	r := make([]byte, n)
 	_, err := rand.Read(r)
 	if err != nil {
-		return nil, err
+		return err
 	}
-	// Allocate shares with space for tag byte
-	_s0 := make([]byte, n+1)
-	_s1 := make([]byte, n+1)
-	_s2 := make([]byte, n+1)
-	// Set tags
-	_s0[0] = 0
-	_s1[0] = 1
-	_s2[0] = 2
-	// Get actual share slices
-	s0 := _s0[1:]
-	s1 := _s1[1:]
-	s2 := _s2[1:]
+	if len(s0) < n {
+		return errors.New("s0 not large enough")
+	}
+	if len(s1) < n {
+		return errors.New("s1 not large enough")
+	}
+	if len(s2) < n {
+		return errors.New("s2 not large enough")
+	}
 	for i := 0; i < n; i++ {
 		s0[i] = ((m[i] & 0xf0) >> 4) ^ r[i]
 		s1[i] = ((m[i] & 0x0f) << 4) ^ r[i]
 		s2[i] = m[i] ^ r[i]
 	}
-	return [][]byte{_s0, _s1, _s2}, nil
+	return nil
 }
 
-// Secrets are joined from any two shares based on the tags used.
-func Join(a, b []byte) ([]byte, error) {
-	// Sort by tags
-	if a[0] > b[0] {
-		a, b = b, a
+// JoinBytes recovers a secret message from two or more shares. Any one of the
+// supplied arrayes can be nil and the secret will still be recovered as long as
+// at least two are used. The reconstructed secret is written to m.
+func JoinBytes(m, s0, s1, s2 []byte) error {
+	if s0 != nil && s1 != nil {
+		return joinBytes01(m, s0, s1)
+	} else if s0 != nil && s2 != nil {
+		return joinBytes02(m, s0, s2)
+	} else if s1 != nil && s2 != nil {
+		return joinBytes12(m, s1, s2)
 	}
-	// Allocate space for assembled message
-	m := make([]byte, len(a)-1)
-	if a[0] == 0 && b[0] == 1 {
-		join01(m, a[1:], b[1:])
-	} else if a[0] == 0 && b[0] == 2 {
-		join02(m, a[1:], b[1:])
-	} else if a[0] == 1 && b[0] == 2 {
-		join12(m, a[1:], b[1:])
-	} else {
-		return nil, errors.New("invalid share combination")
-	}
-	return m, nil
+	return errors.New("insufficient combination of shares")
 }
 
-// When a = s0 and b = s1
+// joinBytes01, when a = s0 and b = s1
 // c = a ^ b
 // m = [c3 c2 c1 c0 0 0 0 0] | [0 0 0 0 c7 c6 c5 c4]
-func join01(m, a, b []byte) {
-	for i := 0; i < len(m); i++ {
+func joinBytes01(m, a, b []byte) error {
+	if len(a) != len(b) {
+		return errors.New("share sizes must match")
+	}
+	if len(m) < len(a) {
+		return errors.New("m not large enough")
+	}
+	for i := 0; i < len(a); i++ {
 		c := a[i] ^ b[i]
 		m[i] = ((c << 4) & 0xf0) | ((c >> 4) & 0x0f)
 	}
+	return nil
 }
 
-// When a = s0 and b = s2
+// joinBytes02, when a = s0 and b = s2
 // c = a ^ b
 // m = [0 0 0 0 c7 c6 c5 c4] ^ c
-func join02(m, a, b []byte) {
-	for i := 0; i < len(m); i++ {
+func joinBytes02(m, a, b []byte) error {
+	if len(a) != len(b) {
+		return errors.New("share sizes must match")
+	}
+	if len(m) < len(a) {
+		return errors.New("m not large enough")
+	}
+	for i := 0; i < len(a); i++ {
 		c := a[i] ^ b[i]
 		m[i] = ((c & 0xf0) >> 4) ^ c
 	}
+	return nil
 }
 
-// When a = s1 and b = s2
+// joinBytes12, when a = s1 and b = s2
 // c = a ^ b
 // m = [c3 c2 c1 c0 0 0 0 0] ^ c
-func join12(m, a, b []byte) {
-	for i := 0; i < len(m); i++ {
+func joinBytes12(m, a, b []byte) error {
+	if len(a) != len(b) {
+		return errors.New("share sizes must match")
+	}
+	if len(m) < len(a) {
+		return errors.New("m not large enough")
+	}
+	for i := 0; i < len(a); i++ {
 		c := a[i] ^ b[i]
 		m[i] = ((c & 0x0f) << 4) ^ c
 	}
+	return nil
 }
--- tshare_test.go
+++ tshare_test.go
@@ -2,70 +2,105 @@ package tshare
 
 import (
 	"bytes"
-	"math/rand"
+	"crypto/rand"
 	"testing"
 )
 
-type testCase struct {
-	Msg    []byte
-	Err    error
-	Shares [][]byte
+func TestJoinBytes(t *testing.T) {
+	m := make([]byte, 1024)
+	rand.Read(m)
+	s0 := make([]byte, len(m))
+	s1 := make([]byte, len(m))
+	s2 := make([]byte, len(m))
+	err := SplitBytes(m, s0, s1, s2)
+	if err != nil {
+		t.Errorf("SplitBytes error: %s", err)
+	}
+	testJoinBytes(t, m, s0, s1, nil)
+	testJoinBytes(t, m, s0, nil, s2)
+	testJoinBytes(t, m, nil, s1, s2)
+	testJoinBytes(t, m, s0, s1, s2)
 }
 
-var testCases []testCase
-
-func init() {
-	for i := 0; i <= 4096; i++ {
-		msg := make([]byte, i)
-		rand.Read(msg)
-		shares, err := Split(msg)
-		testCases = append(testCases, testCase{
-			Msg:    msg,
-			Err:    err,
-			Shares: shares,
-		})
+func testJoinBytes(t *testing.T, m, s0, s1, s2 []byte) {
+	m2 := make([]byte, len(m))
+	err := JoinBytes(m2, s0, s1, s2)
+	if err != nil {
+		t.Errorf("JoinBytes error: %s", err)
+	}
+	if bytes.Compare(m, m2) != 0 {
+		t.Error("JoinBytes incorrect")
 	}
 }
 
-func TestSplit(t *testing.T) {
-	for i, tc := range testCases {
-		if tc.Err != nil {
-			t.Errorf("error testCases[%d]", i)
-		}
-		if len(tc.Shares) != 3 {
-			t.Errorf("incorrect number of shares testCases[%d]", i)
-		}
-		for j := 0; j < 3; j++ {
-			if tc.Shares[j][0] != byte(j) {
-				t.Errorf("bad tag testCases[%d].Shares[%d]", i, j)
-			}
-		}
+func TestJoin01(t *testing.T) {
+	msg := make([]byte, 1024)
+	rand.Read(msg)
+	var mi, mo, s0, s1, s2 bytes.Buffer
+	mi.Write(msg)
+	err := Split(&mi, &s0, &s1, &s2)
+	if err != nil {
+		t.Errorf("Split error: %s", err)
+	}
+	err = Join(&mo, &s0, &s1, nil)
+	if err != nil {
+		t.Errorf("Join error: %s", err)
+	}
+	if bytes.Compare(msg, mo.Bytes()) != 0 {
+		t.Error("Join incorrect")
 	}
 }
 
-func TestJoin(t *testing.T) {
-	// Test Join for each permutation of shares
-	for i := 0; i < 3; i++ {
-		for j := 0; j < 3; j++ {
-			// Exclude case where only one share is used
-			if i == j {
-				continue
-			}
-			testJoin(t, i, j)
-		}
+func TestJoin02(t *testing.T) {
+	msg := make([]byte, 1024)
+	rand.Read(msg)
+	var mi, mo, s0, s1, s2 bytes.Buffer
+	mi.Write(msg)
+	err := Split(&mi, &s0, &s1, &s2)
+	if err != nil {
+		t.Errorf("Split error: %s", err)
+	}
+	err = Join(&mo, &s0, nil, &s2)
+	if err != nil {
+		t.Errorf("Join error: %s", err)
+	}
+	if bytes.Compare(msg, mo.Bytes()) != 0 {
+		t.Error("Join incorrect")
 	}
 }
 
-func testJoin(t *testing.T, i, j int) {
-	for n, tc := range testCases {
-		a := tc.Shares[i]
-		b := tc.Shares[j]
-		m, err := Join(a, b)
-		if err != nil {
-			t.Errorf("Join(s%d, s%d) error testCases[%d]", i, j, n)
-		}
-		if bytes.Compare(m, tc.Msg) != 0 {
-			t.Errorf("Join(s%d, s%d) incorrect testCases[%d]", i, j, n)
-		}
+func TestJoin12(t *testing.T) {
+	msg := make([]byte, 1024)
+	rand.Read(msg)
+	var mi, mo, s0, s1, s2 bytes.Buffer
+	mi.Write(msg)
+	err := Split(&mi, &s0, &s1, &s2)
+	if err != nil {
+		t.Errorf("Split error: %s", err)
+	}
+	err = Join(&mo, nil, &s1, &s2)
+	if err != nil {
+		t.Errorf("Join error: %s", err)
+	}
+	if bytes.Compare(msg, mo.Bytes()) != 0 {
+		t.Error("Join incorrect")
+	}
+}
+
+func TestJoin012(t *testing.T) {
+	msg := make([]byte, 1024)
+	rand.Read(msg)
+	var mi, mo, s0, s1, s2 bytes.Buffer
+	mi.Write(msg)
+	err := Split(&mi, &s0, &s1, &s2)
+	if err != nil {
+		t.Errorf("Split error: %s", err)
+	}
+	err = Join(&mo, &s0, &s1, &s2)
+	if err != nil {
+		t.Errorf("Join error: %s", err)
+	}
+	if bytes.Compare(msg, mo.Bytes()) != 0 {
+		t.Error("Join incorrect")
 	}
 }
